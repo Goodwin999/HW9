feef8a89d37924cee68fb498585ec58b389a5944
Goodwin999 <Vyacheslav.tsitsar@mail.ru>
2023-09-20 02:20:30 +0300

    Fix task 1/Выполнил 1 задачу

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,34 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Home work 9");
+        task1();
+
+    }
+        public static int[] generateRandomArray () {
+            java.util.Random random = new java.util.Random();
+            int[] arr = new int[30];
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = random.nextInt(100_000) + 100_000;
+            }
+            return arr;
+        }
+
+        public static void task1 () {
+            System.out.println("Задача 1");
+            int[] arr = generateRandomArray();
+            int sum = 0;
+            for (int j : arr) {
+                sum = sum + j;
+            }
+            System.out.println("Суммма трат в месяц составляет " + sum + " рублей ");
+
+        }
+
     }
 
-}
+
+
+
+
+
+
+
